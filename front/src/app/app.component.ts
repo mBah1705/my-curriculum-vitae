bbeb457bc5d4b6e9e01b8af95136e8e5d8a15c5a
Mamadou Bah <42182390+mBah1705@users.noreply.github.com>
2024-12-25 18:26:53 +0100

    feat: navigateToGithubPage

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import {MatIconModule, MatIconRegistry} from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 
 type  Mode = 'bright' | 'dark'
@@ -8,7 +11,7 @@ type  Language = 'english' | 'french' | 'dutch'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonToggleModule, FormsModule],
+  imports: [RouterOutlet, MatButtonToggleModule, FormsModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -17,13 +20,27 @@ export class AppComponent {
   title = 'my-curriculum-vitae'
   fontStyle = signal<Mode>('bright')
   language = signal<Language>('french')
- 
+  private readonly _document: Document  = inject(DOCUMENT)
+  private readonly localStorage = this._document.defaultView?.localStorage
+
+  constructor() {
+    const matIconRegistry = inject(MatIconRegistry)
+    const domSanitizer = inject(DomSanitizer)
+    matIconRegistry.addSvgIcon('github', domSanitizer.bypassSecurityTrustResourceUrl('/assets/github.svg'))
+  }
+
   computeMode = computed(() => {
-    localStorage.setItem('theme', this.fontStyle())
-    localStorage['theme'] === 'dark' ? 
+    if(this.localStorage) {
+      localStorage.setItem('theme', this.fontStyle())
+      localStorage['theme'] === 'dark' ? 
       document.documentElement.classList.add('dark') :    
       document.documentElement.classList.remove('dark')
+    }
   })
 
   switchLanguage = computed(() => this.language())
+
+  navigateToGithubPage = () => {
+    window.open('https://github.com/mBah1705/my-curriculum-vitae', "_blank")
+  }
 }
