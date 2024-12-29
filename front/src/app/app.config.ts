9683fb772a62d8f62a203b05dff32531b8582bf7
Mamadou Bah <42182390+mBah1705@users.noreply.github.com>
2024-12-29 13:51:11 +0100

    feat: switch language fr, en, nl works with babel edit

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,15 +1,29 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
+import {TranslateModule, TranslateLoader} from "@ngx-translate/core";
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
+  new TranslateHttpLoader(http, './i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideClientHydration(withEventReplay()), 
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    importProvidersFrom([
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: httpLoaderFactory,
+          deps: [HttpClient]
+        }
+      })
+    ])
   ]
 };
--- front/src/app/header/header.component.ts
+++ front/src/app/header/header.component.ts
@@ -4,26 +4,32 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 type  Mode = 'bright' | 'dark'
-type  Language = 'english' | 'french' | 'dutch'
+type  Language = 'en' | 'fr' | 'nl'
 
 @Component({
   selector: 'app-header',
-  imports: [MatButtonToggleModule, FormsModule, MatIconModule],
+  imports: [MatButtonToggleModule, FormsModule, MatIconModule, TranslateModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   fontStyle = signal<Mode>('bright')
-  language = signal<Language>('french')
+  language = signal<Language>('fr')
   private readonly _document: Document  = inject(DOCUMENT)
   private readonly localStorage = this._document.defaultView?.localStorage
+  private readonly translateService = inject(TranslateService)
 
   constructor() {
     const matIconRegistry = inject(MatIconRegistry)
     const domSanitizer = inject(DomSanitizer)
     matIconRegistry.addSvgIcon('github', domSanitizer.bypassSecurityTrustResourceUrl('/assets/github.svg'))
+
+    this.translateService.addLangs(['fr', 'en', 'nl'])
+    this.translateService.setDefaultLang('fr')
+    this.translateService.use('fr')
   }
 
   computeMode = computed(() => {
@@ -35,7 +41,7 @@ export class HeaderComponent {
     }
   })
 
-  switchLanguage = computed(() => this.language())
+  switchLanguage = computed(() => this.translateService.use(this.language()))
 
   navigateToGithubPage = () => {
     window.open('https://github.com/mBah1705/my-curriculum-vitae', "_blank")
