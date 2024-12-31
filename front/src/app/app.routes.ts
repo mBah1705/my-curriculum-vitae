b43b6ee2ec025d1ab6a904c6ee16e98cc19a428b
Mamadou Bah <42182390+mBah1705@users.noreply.github.com>
2024-12-31 19:07:50 +0100

    feat: languages works! + focus inside the app on active button

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
+import { LanguagesComponent } from './languages/languages.component';
 
 export const routes: Routes = [
     {
+        path: '', 
+        redirectTo: 'languages', 
+        pathMatch: 'full'
+    },
+    {
         path: 'languages',
+        component: LanguagesComponent
     }
 ];
--- front/src/app/content-actions/content-actions.component.ts
+++ front/src/app/content-actions/content-actions.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, OnInit, viewChildren } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import {MatButtonModule} from '@angular/material/button';
-import { RouterLink } from '@angular/router';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-content-actions',
@@ -9,26 +10,49 @@ import { RouterLink } from '@angular/router';
   templateUrl: './content-actions.component.html',
   styleUrl: './content-actions.component.scss'
 })
-export class ContentActionsComponent {
+export class ContentActionsComponent implements OnInit, AfterViewInit {
   actions = [
     {
       title: 'content-actions.professional-experiences',
-      link: '/experiences'
+      link: '/experiences',
     },
     {
       title: 'content-actions.technical-skills',
-      link: '/skills'
+      link: '/skills',
     },
     {
       title: 'content-actions.education-and-trainings',
-      link: '/education'
+      link: '/education',
     },
     {
       title: 'content-actions.languages',
-      link: '/languages'
-    },
-    
-    
-    
+      link: '/languages',
+    },    
   ]
+  
+  private readonly _document: Document  = inject(DOCUMENT)
+  private readonly router = inject(Router)
+
+  btnRefs = viewChildren<ElementRef<HTMLAnchorElement>>('btnRef')
+
+  // The active button is the one representing the displayed component
+  activeBtn: ElementRef<HTMLAnchorElement> | undefined = undefined
+
+  ngOnInit(): void {
+    this._document.addEventListener('click', () => {
+      this.activeBtn?.nativeElement.focus()
+    })
+  }
+  
+  ngAfterViewInit(): void {
+    this.router.events.subscribe(evt => {
+      if (evt instanceof NavigationEnd) {
+        // On navigation focus the button representing the navigation url
+        const activeBtn = this.btnRefs().find(btn => btn.nativeElement.href.endsWith(evt.url))
+        this.activeBtn = activeBtn
+        this.activeBtn?.nativeElement.focus()
+      }
+    })
+  }
+
 }
