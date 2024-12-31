3804dd1af35d70e922763af61b58cc514eda5a55
Mamadou Bah <42182390+mBah1705@users.noreply.github.com>
2024-12-31 15:13:44 +0100

    feat: add languages button

--- front/src/app/content-actions/content-actions.component.ts
+++ front/src/app/content-actions/content-actions.component.ts
@@ -1,17 +1,34 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import {MatButtonModule} from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-content-actions',
-  imports: [TranslateModule, MatButtonModule],
+  imports: [TranslateModule, MatButtonModule, RouterLink],
   templateUrl: './content-actions.component.html',
   styleUrl: './content-actions.component.scss'
 })
 export class ContentActionsComponent {
   actions = [
-    'content-actions.professional-experiences',
-    'content-actions.technical-skills',
-    'content-actions.education-and-trainings'
+    {
+      title: 'content-actions.professional-experiences',
+      link: '/experiences'
+    },
+    {
+      title: 'content-actions.technical-skills',
+      link: '/skills'
+    },
+    {
+      title: 'content-actions.education-and-trainings',
+      link: '/education'
+    },
+    {
+      title: 'content-actions.languages',
+      link: '/languages'
+    },
+    
+    
+    
   ]
 }
