20a2e550f3a7f584e1d2f03a6fd26c6a6ecaf247
Mamadou Bah <42182390+mBah1705@users.noreply.github.com>
2025-01-02 13:44:12 +0100

    feat: languages component and translations (#6)

--- front/src/app/languages/languages.component.ts
+++ front/src/app/languages/languages.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+import { ProgressComponent } from '../../ui/progress/progress.component';
 
 @Component({
   selector: 'app-languages',
-  imports: [],
+  imports: [TranslatePipe, ProgressComponent],
   templateUrl: './languages.component.html',
   styleUrl: './languages.component.scss'
 })
 export class LanguagesComponent {
+  languages = [
+    {item: 'languages.french', value: 100},
+    {item: 'languages.dutch', value: 95},
+    {item: 'languages.english', value: 90},
+  ]
 }
