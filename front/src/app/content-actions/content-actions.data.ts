083cb78e82bd2de4d42ca8a899f5e9f6d3c03a05
Mamadou Bah <42182390+mBah1705@users.noreply.github.com>
2025-01-05 20:30:41 +0100

    feat: experiences component and service

---
+++ front/src/app/content-actions/content-actions.data.ts
@@ -0,0 +1,19 @@
+export const CONTENT_ACTIONS_DATA = [
+    {
+      title: 'content-actions.professional-experiences',
+      link: '/experiences',
+    },
+    {
+      title: 'content-actions.technical-skills',
+      link: '/skills',
+    },
+    {
+      title: 'content-actions.education-and-trainings',
+      link: '/education',
+    },
+    {
+      title: 'content-actions.languages',
+      link: '/languages',
+    },    
+  ]
+  
