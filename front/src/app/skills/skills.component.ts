b4bae0d502b257f368c003890cc5d8ded39cd840
Mamadou Bah <42182390+mBah1705@users.noreply.github.com>
2025-01-07 23:52:53 +0100

    chore: undoing change on frontend

--- front/src/app/skills/skills.component.ts
+++ front/src/app/skills/skills.component.ts
@@ -12,24 +12,24 @@ export interface Skill {
   challenges?: string[]
 }
 
-// const ELEMENT_DATA: Skill[] = [
-//   {skill: 'Angular', theoryWeight: 90, practicalWeight: 90, usedInThisApp: true, challenges: ['RxJS, ng-19', 'Angular Material CDK']},
-//   {skill: 'Typescript', theoryWeight: 80, practicalWeight: 85, usedInThisApp: true},
-//   {skill: 'Javascript', theoryWeight: 80, practicalWeight: 70, usedInThisApp: true},
-//   {skill: 'Git', theoryWeight: 60, practicalWeight: 65, usedInThisApp: true, challenges: ['merge', 'rebase', 'cherry-pick', 'revert']},
-//   {skill: 'TailwindCSS', theoryWeight: 70, practicalWeight: 75, usedInThisApp: true, challenges: ['Using preprocessors', 'Optimizing for production', 'Manual tweacking']},
-//   {skill: 'Angular Material', theoryWeight: 75, practicalWeight: 75, usedInThisApp: true},
-//   {skill: 'RxJS', theoryWeight: 75, practicalWeight: 55, usedInThisApp: true, challenges: ['To master operators']},
-//   {skill: 'NgRx', theoryWeight: 70, practicalWeight: 55, usedInThisApp: false, challenges: ['To master the pattern']},
-//   {skill: 'NodeJS', theoryWeight: 65, practicalWeight: 30, usedInThisApp: true, challenges: ['Event programming']},
-//   {skill: 'Unit Tests', theoryWeight: 85, practicalWeight: 85, usedInThisApp: true},
-//   {skill: 'Express', theoryWeight: 60, practicalWeight: 25, usedInThisApp: true, challenges: ['middlewares', 'templating', 'routing']},
-//   {skill: 'HTML/CSS', theoryWeight: 85, practicalWeight: 90, usedInThisApp: true},
-//   {skill: 'npm/pnpm', theoryWeight: 55, practicalWeight: 50, usedInThisApp: true},
-//   {skill: 'ESLint/Prettier', theoryWeight: 75, practicalWeight: 70, usedInThisApp: false},
-//   {skill: 'SSR/SSG (Angular)', theoryWeight: 45, practicalWeight: 55, usedInThisApp: true},
-//   {skill: 'PWA (Angular)', theoryWeight: 55, practicalWeight: 65, usedInThisApp: false},
-// ];
+const ELEMENT_DATA: Skill[] = [
+  {skill: 'Angular', theoryWeight: 90, practicalWeight: 90, usedInThisApp: true, challenges: ['RxJS, ng-19', 'Angular Material CDK']},
+  {skill: 'Typescript', theoryWeight: 80, practicalWeight: 85, usedInThisApp: true},
+  {skill: 'Javascript', theoryWeight: 80, practicalWeight: 70, usedInThisApp: true},
+  {skill: 'Git', theoryWeight: 60, practicalWeight: 65, usedInThisApp: true, challenges: ['merge', 'rebase', 'cherry-pick', 'revert']},
+  {skill: 'TailwindCSS', theoryWeight: 70, practicalWeight: 75, usedInThisApp: true, challenges: ['Using preprocessors', 'Optimizing for production', 'Manual tweacking']},
+  {skill: 'Angular Material', theoryWeight: 75, practicalWeight: 75, usedInThisApp: true},
+  {skill: 'RxJS', theoryWeight: 75, practicalWeight: 55, usedInThisApp: true, challenges: ['To master operators']},
+  {skill: 'NgRx', theoryWeight: 70, practicalWeight: 55, usedInThisApp: false, challenges: ['To master the pattern']},
+  {skill: 'NodeJS', theoryWeight: 65, practicalWeight: 30, usedInThisApp: true, challenges: ['Event programming']},
+  {skill: 'Unit Tests', theoryWeight: 85, practicalWeight: 85, usedInThisApp: true},
+  {skill: 'Express', theoryWeight: 60, practicalWeight: 25, usedInThisApp: true, challenges: ['middlewares', 'templating', 'routing']},
+  {skill: 'HTML/CSS', theoryWeight: 85, practicalWeight: 90, usedInThisApp: true},
+  {skill: 'npm/pnpm', theoryWeight: 55, practicalWeight: 50, usedInThisApp: true},
+  {skill: 'ESLint/Prettier', theoryWeight: 75, practicalWeight: 70, usedInThisApp: false},
+  {skill: 'SSR/SSG (Angular)', theoryWeight: 45, practicalWeight: 55, usedInThisApp: true},
+  {skill: 'PWA (Angular)', theoryWeight: 55, practicalWeight: 65, usedInThisApp: false},
+];
 
 
 @Component({
