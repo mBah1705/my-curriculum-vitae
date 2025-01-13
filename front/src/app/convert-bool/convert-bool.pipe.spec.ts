9786b4d4ea74a1befeef1e3e621f748c8acc9b6c
Mamadou Bah <42182390+mBah1705@users.noreply.github.com>
2025-01-13 19:31:40 +0100

    feat: tests for convert-bool pipe

--- front/src/app/convert-bool/convert-bool.pipe.spec.ts
+++ front/src/app/convert-bool/convert-bool.pipe.spec.ts
@@ -5,4 +5,19 @@ describe('ConvertBoolPipe', () => {
     const pipe = new ConvertBoolPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should display "skills-table.yes" for true value', () => {
+    const pipe = new ConvertBoolPipe();
+    expect(pipe.transform(true)).toBe('skills-table.yes');
+  });
+
+  it('should display "skills-table.no" for false value', () => {
+    const pipe = new ConvertBoolPipe();
+    expect(pipe.transform(false)).toBe('skills-table.no');
+  });
+
+  it('should display "skills-table.no" for undefined value', () => {
+    const pipe = new ConvertBoolPipe();
+    expect(pipe.transform(undefined)).toBe('skills-table.no');
+  });
 });
