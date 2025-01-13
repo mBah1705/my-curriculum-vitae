8e394ea09e050ca0b70615b6c4a37a453cbabe16
Mamadou Bah <42182390+mBah1705@users.noreply.github.com>
2025-01-13 17:47:13 +0100

    feat: tests for progress component

--- front/src/ui/progress/progress.component.spec.ts
+++ front/src/ui/progress/progress.component.spec.ts
@@ -1,23 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { ProgressComponent } from './progress.component';
+import { ComponentRef } from '@angular/core';
 
 describe('ProgressComponent', () => {
   let component: ProgressComponent;
+  let componentRef: ComponentRef<ProgressComponent>;
   let fixture: ComponentFixture<ProgressComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProgressComponent]
-    })
-    .compileComponents();
+      declarations: [],
+      imports: [MatProgressBarModule, ProgressComponent]
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(ProgressComponent);
     component = fixture.componentInstance;
+    componentRef = fixture.componentRef
+    componentRef.setInput('value', 75)
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a default value', () => {
+    expect(component.value).toBeDefined();
+  });
+
+  it('should compute widthStyle correctly', () => {
+    fixture.detectChanges();
+    expect(component.widthStyle()).toBe('75%');
+  });
+
+  it('should update widthStyle when value changes', () => {
+    componentRef.setInput('value', 30)
+    fixture.detectChanges();
+    expect(component.widthStyle()).toBe('30%');
   });
 });
