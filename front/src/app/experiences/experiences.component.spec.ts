89b6c1c2f177783321bbffd1d62f84bc4d2852e6
Mamadou Bah <42182390+mBah1705@users.noreply.github.com>
2025-01-13 19:53:18 +0100

    feat: tests for education component

--- front/src/app/experiences/experiences.component.spec.ts
+++ front/src/app/experiences/experiences.component.spec.ts
@@ -1,14 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { ExperiencesComponent } from './experiences.component';
+import { avivAngularExperience, avivReactExperience, cvAppExperience, qDelftExperience } from './experiences.data';
 
 describe('ExperiencesComponent', () => {
   let component: ExperiencesComponent;
   let fixture: ComponentFixture<ExperiencesComponent>;
+  const experiencesServiceStub = {
+    experiences: [],
+    addExperience: () => {},
+    resetExperiences: () => {},
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ExperiencesComponent]
+      imports: [ExperiencesComponent, TranslateModule.forRoot()],
+      providers: [{ provide: ExperiencesComponent, useValue: experiencesServiceStub }],
     })
     .compileComponents();
 
@@ -19,5 +27,26 @@ describe('ExperiencesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should add experiences on init', () => {
+    const addExperienceSpy = spyOn(component.experiencesBuilder, 'addExperience');
+    component.ngOnInit();
+    expect(addExperienceSpy).toHaveBeenCalledTimes(4);
+  });
+
+  it('should call addExperience with all experiences', () => {
+    const addExperienceSpy = spyOn(component.experiencesBuilder, 'addExperience');
+    component.ngOnInit();
+    expect(addExperienceSpy).toHaveBeenCalledWith(cvAppExperience);
+    expect(addExperienceSpy).toHaveBeenCalledWith(avivAngularExperience);
+    expect(addExperienceSpy).toHaveBeenCalledWith(qDelftExperience);
+    expect(addExperienceSpy).toHaveBeenCalledWith(avivReactExperience);
+  });
+
+  it('should reset experiences on destroy', () => {
+    const resetExperiencesSpy = spyOn(component.experiencesBuilder, 'resetExperiences');
+    component.ngOnDestroy();
+    expect(resetExperiencesSpy).toHaveBeenCalled();
   });
 });
