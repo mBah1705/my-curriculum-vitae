0c3f7b37c9d81f4a7157a7febc8a6eaf7171bc51
Mamadou Bah <42182390+mBah1705@users.noreply.github.com>
2025-01-13 17:06:51 +0100

    feeat: begin of tests for content-actions component

--- front/src/app/content-actions/content-actions.component.spec.ts
+++ front/src/app/content-actions/content-actions.component.spec.ts
@@ -1,14 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ContentActionsComponent } from './content-actions.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { MatButtonModule } from '@angular/material/button';
+import { By } from '@angular/platform-browser';
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
+import { provideRouter } from '@angular/router';
 
-describe('ContentActionsComponent', () => {
+fdescribe('ContentActionsComponent', () => {
   let component: ContentActionsComponent;
   let fixture: ComponentFixture<ContentActionsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ContentActionsComponent]
+      imports: [
+        TranslateModule.forRoot(),
+        MatButtonModule,
+        ContentActionsComponent,
+      ],
+      providers: [
+        provideRouter([]),
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
 
@@ -19,5 +31,25 @@ describe('ContentActionsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have actions defined', () => {
+    expect(component.actions).toBeDefined();
+    expect(component.actions.length).toBeGreaterThan(0);
+  });
+
+  it('should render buttons for each action', () => {
+    const buttons: DebugElement[] = fixture.debugElement.queryAll(By.css('a[type="button"]'));
+    console.log(fixture.debugElement);
+    
+    expect(buttons.length).toBe(component.actions.length);
+  });
+
+  it('should unsubscribe from router events on destroy', () => {
+    if(component.subscription) {
+      spyOn(component.subscription, 'unsubscribe');
+    }
+    component.ngOnDestroy();
+    expect(component.subscription?.unsubscribe).toHaveBeenCalled();
   });
 });
