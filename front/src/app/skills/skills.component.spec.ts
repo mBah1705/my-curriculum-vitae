2a672051ea4c48487ede41c3ec470e5e085f630f
Mamadou Bah <42182390+mBah1705@users.noreply.github.com>
2025-01-14 14:46:29 +0100

    feat: tests for skills component

--- front/src/app/skills/skills.component.spec.ts
+++ front/src/app/skills/skills.component.spec.ts
@@ -1,14 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SkillsComponent } from './skills.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { SkillsService } from './skills.service';
+import { signal } from '@angular/core';
+import { Skill } from './skill.model';
 
-describe('SkillsComponent', () => {
+fdescribe('SkillsComponent', () => {
   let component: SkillsComponent;
   let fixture: ComponentFixture<SkillsComponent>;
 
+  const skillsServiceStub = {
+    fetchSkills: () => {
+      return signal<Skill[]>([{
+        skill: 'test-skill',
+        theoryWeight: 1,
+        practicalWeight: 1,
+        usedInThisApp: true,
+        challenges: ['test-challenge']
+      }]);
+    }
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SkillsComponent]
+      imports: [SkillsComponent, TranslateModule.forRoot()],
+      providers: [{ provide: SkillsService, useValue: skillsServiceStub }]
     })
     .compileComponents();
 
@@ -19,5 +36,15 @@ describe('SkillsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should fetch skills', () => {
+    expect(component.dataSource()).toEqual([{
+      skill: 'test-skill',
+      theoryWeight: 1,
+      practicalWeight: 1,
+      usedInThisApp: true,
+      challenges: ['test-challenge']
+    }]);
   });
 });
