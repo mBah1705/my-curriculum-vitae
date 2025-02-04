7edf9f8f8a43e4e737323b1e6744441e6adccb2e
Mamadou Bah <42182390+mBah1705@users.noreply.github.com>
2025-02-04 15:12:37 +0100

    Feat/retire backend call (#18)
    * Feat: add skills data and refactor skills service to use local data
    
    * Refactor: simplify SkillsService tests and use local skills data

--- front/src/app/skills/skills.service.ts
+++ front/src/app/skills/skills.service.ts
@@ -1,30 +1,10 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { Skill } from './skill.model';
-import { catchError, throwError } from 'rxjs';
+import { Injectable } from '@angular/core';
+import {  of } from 'rxjs';
+import { SKILLS_DATA } from './skills.data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SkillsService {
-  private readonly url = 'https://my-curriculum-vitae-g5sp.onrender.com'
-
-  httpClient = inject(HttpClient)
-
-  readonly fetchSkills$ = () => this.httpClient.get<Skill[]>(this.url).pipe(
-    catchError((error: HttpErrorResponse) => {
-      if(error.status === 0) {
-        // A client-side or network error occurred. Handle it accordingly.
-        console.error('An error occurred:', error.error)
-      } else {
-        // The backend returned an unsuccessful response code.
-        // The response body may contain clues as to what went wrong.
-        console.error(
-          `Backend returned code ${error.status}, body was: `, error.error);
-      }
-
-      // Return an observable with a user-facing error message.
-      return throwError(() => new Error('Something bad happened; please try again later.'))
-    })
-  )
+   readonly fetchSkills$ = () => of(SKILLS_DATA)
 }
